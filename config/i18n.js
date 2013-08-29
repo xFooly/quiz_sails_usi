bf9034858286aa0837d15e7f47a8990a532df57b
xFooly <andrea.gallidabino@gmail.com>
2013-08-29 18:50:37 +0200

    keyboard

--- config/i18n.js
+++ config/i18n.js
@@ -1,7 +1,7 @@
 module.exports.i18n = {
 
   // Which locales are supported?
-  locales: ['en', 'es'],
+  locales: ['en', 'de', 'it'],
 
     // Where are your locale translations located?
   localesDirectory: '/config/locales'
