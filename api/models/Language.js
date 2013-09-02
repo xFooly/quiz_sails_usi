aaab0ce88fecc15e6ff7c1ad258472a270f62313
xFooly <andrea.gallidabino@gmail.com>
2013-09-02 22:16:20 +0200

    nuovo welcome

--- api/models/Language.js
+++ api/models/Language.js
@@ -27,20 +27,20 @@ module.exports = {
         maxLength: 2
     }
     
-  },
-
-  afterCreate: function (language, callback) {
-      fs.mkdir('./assets/locales/' + language.code, function(err) {
-         fs.open('./assets/locales/' + language.code + '/translation.json', 'w', function(err, fd){
-            var json = {}
-            json['welcome'] = {}
-            json['welcome']['title'] = "Conosciamoci."
-            fs.write(fd, JSON.stringify(json), null, 'utf8', function(){
-                fs.close(fd, function(){
-                    callback()
-                });
-            });
-         })
-      })
   }
+
+//  afterCreate: function (language, callback) {
+//     Language.find().done(function (err, languages) {
+//         fs.open('./config/locales/' + language.code + '.json', 'w', function(err, fd){
+//             var json = {}
+//             json['welcome'] = {}
+//             json['welcome']['title'] = "Conosciamoci."
+//             fs.write(fd, JSON.stringify(json), null, 'utf8', function(){
+//                 fs.close(fd, function(){
+//                     callback()
+//                 });
+//             });
+//         })
+//     })
+//  }
 };
